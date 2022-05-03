a8a6ef109a7180be2c3adc30ce738667394bbb9e
eswar <neelaveni.eswar04@gmail.com>
2022-05-04 02:31:51 +0530

    [Eswar] Welcome to DataStructures And Algorithms

---
+++ DatastructuresAndAlgorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatastructuresAndAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Datastructures and Algorithms");
+            Console.ReadLine();
+        }
+    }
+}
